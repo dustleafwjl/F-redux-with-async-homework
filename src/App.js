8318dec5ee6c4f23593350c28b0960241b185596
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-23 17:08:59 +0800

    [jianglin] feat: add loadingmask render component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import Header from './layout/Header';
 import Menu from './layout/Menu';
 import Home from './views/Home';
 import { modules } from './utils';
+
+import loadingImg from './assets/loading.jpg';
+import loginFailed from './assets/loginFailed.jpg';
+
 import './App.scss';
 
 class App extends Component {
   render() {
+    console.log(this.props.userInfo.fetchStatus);
     return (
       <div className="app">
         <Router>
@@ -34,10 +39,26 @@ class App extends Component {
             </Switch>
           </main>
         </Router>
+        {this.props.userInfo.fetchStatus === 'pending' ? <LoadingMask /> : null}
       </div>
     );
   }
 }
+
+const LoadingMask = () => {
+  const loadingMaskStyle = {
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    fontSize: '36px',
+    color: '#fff',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  };
+  return <div style={loadingMaskStyle}>loading...</div>;
+};
 
 const mapStateToProps = ({ userInfo }) => ({
   userInfo
